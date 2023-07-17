5075ac635a0873e14d25f650e9b5d1e34fd13ca5
Joshua Anton <janto085@uottawa.ca>
2023-07-17 15:10:28 -0400

    Mutation for python

---
+++ Test/test_source_counter.py
@@ -0,0 +1,19 @@
+import unittest
+
+from score_counter import ScoreCounter
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_score_counter(self):
+        self.assertEqual(ScoreCounter.increase_score(4, 0))
+
+    def test_draw(self):
+        self.assertEqual(ScoreCounter.draw(2, 3))
+
+    def test_get_circle(self):
+        self.assertEqual(ScoreCounter.get_circles_eaten(3, 3))
+
+
+if __name__ == '__main__':
+    unittest.main()
